1669dfe0ff69a7a06422da6395a3f1a395ccebae
mastertwig <henry.richard.j@gmail.com>
2017-08-28 21:44:47 +1000

    navmesh scene

--- Project Minimum/Assets/Prototyping/Scripts/CameraControllerTest.cs
+++ Project Minimum/Assets/Prototyping/Scripts/CameraControllerTest.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraControllerTest : MonoBehaviour
 {
     // Camera target to look at.
     public Transform target;

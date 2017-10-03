fe9709ccaf4afcc84daab8b6df84d8786ac2f1e8
mastertwig <henry.richard.j@gmail.com>
2017-10-03 20:26:52 +1100

    first real project
    caine's base project

---
+++ Project Minimum/Assets/Scripts/Mouse_Over.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Mouse_Over : MonoBehaviour
+{
+
+    public string myString;
+    public Text labelText;
+    public float fadeTime;
+    public bool displayLabel;
+
+
+    void Update()
+    {
+        FadeText();
+    }
+        
+    void OnMouseOver()
+    {
+        displayLabel = true;
+    }
+
+    void OnMouseExit()
+    {
+        displayLabel = false;
+    }
+
+    void FadeText ()
+    {
+        if (displayLabel)
+        {
+            labelText.text = myString;
+            labelText.color = Color.Lerp(labelText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            labelText.color = Color.Lerp(labelText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+    }
+
+
+}

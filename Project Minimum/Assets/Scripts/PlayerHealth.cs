3066caf4231032989e513d1635de79df5ffb2b3e
Pastryproducts <mmmmmmchips@hotmail.com>
2017-11-04 15:08:49 +1100

    1111

---
+++ Project Minimum/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Image damageImage;
+    public AudioClip deathClip;
+        
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    bool isDead;
+    bool damaged;
+
+
+    void Awake ()
+    {
+        playerAudio = GetComponent <AudioSource> ();
+        playerMovement = GetComponent <PlayerMovement> ();
+        currentHealth = startingHealth;
+    }
+
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        playerAudio.Play ();
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+
+        playerMovement.enabled = false;
+
+        RestartLevel();
+    }
+
+
+    public void RestartLevel ()
+    {
+        SceneManager.LoadScene (0);
+    }
+}
